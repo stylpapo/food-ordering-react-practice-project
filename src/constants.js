b224445cafe348023258dc04ad769b50f5283ffc
unknown <stelios.papoutsakis@altenar.com>
2024-12-03 13:43:50 +0200

    set remote server for local development

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,1 @@
-// check environment to load proper endpoint
-export const server = window.location.href.includes("localhost") ? "http://localhost:3000" : "https://food-order-react-app-backend-1011034156124.europe-west1.run.app";
+export const server = "https://food-order-react-app-backend-1011034156124.europe-west1.run.app";
