793fabe99eb88c127488856c7cdc2309888c2fb6
unknown <stelios.papoutsakis@altenar.com>
2024-12-03 02:18:51 +0200

    remove backend & set remote backend endpoint

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const server = "http://localhost:3000"; // I would like something more dynamic here for the base URL
+// check environment to load proper endpoint
+export const server = window.location.href.includes("localhost") ? "http://localhost:3000" : "https://food-order-react-app-backend-1011034156124.europe-west1.run.app";
